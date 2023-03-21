2fdec06eb2dcd6afbfa32c20d3ad25f1a2855cc5
Chava <schavarria13@alumnos.uaq.mx>
2023-03-21 15:11:15 -0600

    State in Hook and API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useEffect} from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
@@ -24,14 +24,23 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-  const [ SelectMonedas ] = useSelectMonedas("Elige tu Moneda", monedas)
+  const [ moneda, SelectMonedas ] = useSelectMonedas("Elige tu Moneda", monedas)
   
+  useEffect(()=>{
+    const consultarAPI = async ()=>{
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json()
+      console.log(resultado.Data);
+    }
+    consultarAPI();
+  }, [])
   
   return (
     <form>
 
         <SelectMonedas />
-        
 
         <InputSubmit
             type="submit"
